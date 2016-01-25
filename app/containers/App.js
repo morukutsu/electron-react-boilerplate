01c090f18ecec53225918e18520ec5dc7c7dc313
Morukutsu <ulderic.k@free.fr>
2016-01-26 00:28:29 +0100

    added gl-react

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,23 +1,43 @@
 import React, { Component, PropTypes } from 'react';
+import GL                              from 'gl-react';
+import { Surface }                     from 'gl-react-dom'; // in React DOM context
+
+const shaders = GL.Shaders.create({
+    helloGL: {
+        frag: `
+        precision highp float;
+        varying vec2 uv;
+        uniform float blue;
+        void main () {
+            gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+        }`
+    }
+});
 
 export default class App extends Component {
-  static propTypes = {
-    children: PropTypes.element.isRequired
-  };
+    static propTypes = {
+        children: PropTypes.element.isRequired
+    };
 
-  render() {
-    return (
-      <div>
-        {this.props.children}
-        {
-          (() => {
-            if (process.env.NODE_ENV !== 'production') {
-              const DevTools = require('./DevTools');
-              return <DevTools />;
-            }
-          })()
-        }
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Surface width={511} height={341}>
+                    <GL.Node
+                        shader={shaders.helloGL}
+                        uniforms={{blue: 1.0}}
+                    />
+                </Surface>
+
+                {
+                    (() => {
+                        if (process.env.NODE_ENV !== 'production') {
+                            const DevTools = require('./DevTools');
+                            return <DevTools />;
+                        }
+                    })()
+                }
+            </div>
+        );
+    }
 }
